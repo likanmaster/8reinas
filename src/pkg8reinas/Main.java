57bc17e54a9480ee402d4cd252f8df234d66ae5d
likanmaster <jlicancura@ing.ucsc.cl>
2019-09-02 16:35:07 -0300

    Update Main.java

--- src/pkg8reinas/Main.java
+++ src/pkg8reinas/Main.java
@@ -14,13 +14,35 @@ import java.math.*;
  * @author likanmaster
  */
 public class Main {
+Scanner sc = new Scanner(System.in);   
 
     /**
      * @param args the command line arguments
      */
+    public void randomceroyuno() {//funcion para generar un numer random  entre 0 y 1
+    
+            
+        int tablero =(int)(Math.random());
+        System.out.println(tablero); 
+    }
+
+   
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("hola mundo");
+        int[] tablero = new int[8];
+        for (int i=0;i<8;i++){//poblaciones
+            for (int j=0;j<8;j++){
+                  tablero[i] =(int)(Math.random()*10);
+                  System.out.print(" ");
+                  System.out.print(tablero[i]);
+             }
+             System.out.println();
+        }
+       
+        
+   
+        
     }
+    
     
 }
