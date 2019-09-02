ba88e928f1cf5295e363de0d652866ed82a3bc36
likanmaster <jlicancura@ing.ucsc.cl>
2019-09-02 15:31:55 -0300

    holamundo

--- src/pkg8reinas/Main.java
+++ src/pkg8reinas/Main.java
@@ -1,9 +1,13 @@
+package pkg8reinas;
+import java.util.*;
+import java.io.*;
+import java.math.*;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pkg8reinas;
+
 
 /**
  *
@@ -16,6 +20,7 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("hola mundo");
     }
     
 }
