e58176be1293f0d6fd18396d3e6691275ffea87d
likanmaster <38567416+likanmaster@users.noreply.github.com>
2019-09-19 22:45:19 -0300

    Update Main.java

--- src/pkg8reinas/Main.java
+++ src/pkg8reinas/Main.java
@@ -1,7 +1,5 @@
 package pkg8reinas;
 import java.util.*;
-import java.io.*;
-import java.math.*;
 import java.util.ArrayList;
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -14,30 +12,28 @@ import java.util.ArrayList;
  *
  * @author likanmaster
  */
-public class Main {
-//Scanner sc = new Scanner(System.in);   
-  
-    public static int randomabin(int semilla){ //numero randomico entre 0 y uno. depende de la semilla
-  Random r = new Random();
-      int valorDado = r.nextInt(2);  // Entre 0 y 1.
-    return valorDado;
+public class Main { 
+  //numero randomico entre 0 y uno. depende de la semilla
+    public static int randomabin(int semilla){ 
+      Random r = new Random();
+      int valor = r.nextInt(2);  // Entre 0 y 1.
+    return valor;
    }
-  
-    public static double randomadec(int a, int b, int semilla){ // random con rango de [a b](decimales)
-    Random rnd = new Random(semilla);
-    Double rndInd = b + ( a - b ) * rnd.nextDouble();
-    
-    return rndInd;
-   
+    // random con rango de [a b](decimales)
+    public static double randomadec(int a, int b, int semilla){ 
+    Random r = new Random(semilla);
+    Double ri = b + ( a - b ) * r.nextDouble(); 
+    return ri;
    }
-    public static int randomanint(int a, int b, int semilla){ //  random con rago de [a b](enter0)
+    //  random con rago de [a b](enter0)
+    public static int randomanint(int a, int b, int semilla){ 
     Random rnd = new Random(semilla);
     int rndInd = (int)(b + ( a - b ) * rnd.nextDouble());
     return rndInd;
    }
- 
+ // creamos array de a - b onrdenados aleatoriamente sin repetirse
     public static int[] RandomizeArray(int a, int b){
-		Random rgen = new Random();  // creamos array de a - b onrdenados aleatoriamente sin repetirse 	
+		Random rgen = new Random();   	
 		int size = b-a+1;
 		int[] array = new int[size];
  
@@ -56,21 +52,21 @@ public class Main {
  
 		return array;
 	}
-    public static int fittnes(int [] tablero){  // funcion de fittnes para obtener la cantidad de choques
+    // funcion de fittnes para obtener la cantidad de choques
+    public static int fittnes(int [] tablero){  
     int cantidadchoques=0;          // si hay dos numeros iguales,suma un choque
         for(int i=0;i<tablero.length;i++){  
             for(int j=0;j<tablero.length;j++){
                 if((i-tablero[i]==j-tablero[j])||(i+tablero[i])==j+tablero[j]){
                  cantidadchoques++;
-                
                 }
             }
         }     
         cantidadchoques=Math.abs(cantidadchoques-tablero.length);
     return cantidadchoques;
     }
-  
-    public static int ruolette(ArrayList<Integer> fitvalores, ArrayList<int[]> poblacion, int semilla){  // funcion de ruleta para elegir un tableros
+  // funcion de ruleta para elegir un tableros
+    public static int ruolette(ArrayList<Integer> fitvalores, ArrayList<int[]> poblacion, int semilla){  
         int totalfitnes = 0;
         int totalfitnesnuevo = 0;
         int indtablero = 0;
@@ -101,10 +97,7 @@ public class Main {
          nproporcion[i]=pro;
          //System.out.println("nueva proporcion "+nproporcion[i]);
            } 
-     
-     
      ruleta.add(nproporcion[0]);
-    
     // System.out.println("primera propporniaso "+nproporcion[0]);
        for(int i=1;i<fitvalores.size();i++){  //poner datos en la ruleta
         double var1=ruleta.get(i-1);
@@ -112,11 +105,8 @@ public class Main {
            ruleta.add(var1+var2);
         //System.out.println("ruleta "+ruleta.get(i));
           }
-   
-     
         double num=randomadec(0,1,semi);
-        // System.out.println("numero random dec   "+num);
-        
+        // System.out.println("numero random dec   "+num);       
          for(int i=0;i<fitvalores.size();i++){  //poner datos en el array proporcion
         // System.out.println(ruleta.get(i));
             if(num<ruleta.get(i)){
@@ -124,36 +114,26 @@ public class Main {
               //System.out.println("se guardo el tablero   "+indtablero);
               break;
             }
-      
           }
-         
        return indtablero;//esto devuelve el indice del tablero  ganador en la ruleta
     }
-    
-    private static int[] obtenerpadre(ArrayList<int[]> poblacion, int ttablero, int padre1) {
+    //funcion para obtener eel vector padre dado el indice de ubicacion en el array
+    public static int[] obtenerpadre(ArrayList<int[]> poblacion, int ttablero, int padre1) {
         int[] padre = new int[ttablero];
-       
         for(int i=0;i<poblacion.size();i++){  //total de fitnes
-            
                // System.out.println("sapos qls"+poblacion.get(i));
                if (i==padre1) {
-                System.out.println("padre "+Arrays.toString(poblacion.get(i)));
+                //System.out.println("padre "+Arrays.toString(poblacion.get(i)));
                 padre=poblacion.get(i);
-               
-            }
-                
+            }    
             //for (int j = 0; j<=ttablero ; j++) {
               //   System.out.print("padre  em vec"+padre[j]);
             //}
-            
-              
         }
-    
-       return padre;//esto devuelve el indice del tablero  ganador en la ruleta //To change body of generated methods, choose Tools | Templates.
+       return padre;
     }
-    
+    //funcion booleana para saber si se cruzan 
     public static boolean probabilidadcruza(double pcruza, double randomdec){
-        
        boolean pcruzas;
         pcruzas = false;
         if(pcruza > randomdec){
@@ -164,24 +144,22 @@ public class Main {
         }
         return pcruzas;
     }
-    
-    public static int [] cruza(int[] padre,int[]madre, int ttablero, int mitad, int con){
-       //esta funcion recibe al padre , la madre, el tamaño del tablero, el numero de corte, y una condicion que dice si queremos el primer o segundo hijo 
+    //esta funcion recibe al padre , la madre, el tamaño del tablero, el numero de corte, y una condicion que dice si queremos el primer o segundo hijo
+    public static int [] cruza(int[] padre,int[]madre, int ttablero, int mitad, int con){ 
        int [] hijo=new int[ttablero+1];    
        int tam=ttablero+1;  
-     //  System.out.println("tamaño tablero "+tam);
-       int mid = mitad; //
+       int mid = mitad;
          //System.out.println("mitad "+mitad);
        int[] padreaux=padre;
        int[] madreaux=madre;
-       for (int i = 0; i < tam; i++) {
+       //for (int i = 0; i < tam; i++) {
          //  System.out.print("v"+madreaux[i]);
-        }
-        for (int i = 0; i <tam; i++) {
-            hijo[i]=7;
+        //}
+       // for (int i = 0; i <tam; i++) {
+          //  hijo[i]=7;
           //  System.out.print("h"+hijo[i]);
            // System.out.print(madreaux[i]);
-        }
+        //}
         //System.out.println("ijo con ceros "+hijo);
            if (con==1) {
             for(int i=0; i<mid;i++){
@@ -203,21 +181,20 @@ public class Main {
                 hijo[i]=padre[i];
              // System.out.println("recibe de la madre el "+hijo[i]);
            }
-          }
-           
+          } 
            return hijo;
     } 
-      
+    //funcion de prueba que repara un hijo si este tiene dos numeros iguales, es decir choques verticales
     public static int[]reparacion(int[] hijo,int ttablero){
        int []hijor=new int[ttablero];
        int falta;
        int cont=0;
        int aux;
        int repite;
-        ArrayList<Integer> repetidos = new ArrayList<>();
-        ArrayList<Integer> faltantes = new ArrayList<>();
-        for(int i=0;i<=ttablero;i++){
-        aux=hijo[i];
+       ArrayList<Integer> repetidos = new ArrayList<>();
+       ArrayList<Integer> faltantes = new ArrayList<>();
+       for(int i=0;i<=ttablero;i++){
+       aux=hijo[i];
             for(int j=0;j<=ttablero;j++){
                   if(aux==hijo[j]){
                    cont=cont+1;
@@ -286,11 +263,12 @@ public class Main {
         
        return hijor;
     }
+   //funcion que repara un hijo si este tiene dos numeros iguales, esta si, es decir choques verticales
     public static int[]reparacion2(int[] hijo,int ttablero){
     int[] hijor= RandomizeArray(0, ttablero);
        return hijor;
     }
-   
+   // funcion que intercambia dos numeros en un vector
     private static int[] mutacion(int[] hijo1, int ttablero,int num1,int num2, double prob,double pmutacion) {
            int aux;
         if (prob<pmutacion) {
@@ -302,107 +280,137 @@ public class Main {
             }
         }    
         }
-        
      return hijo1;
     }
-     
-    
+     //
+    public static void dibujo(int [] tablero){
+     for(int i=0; i < tablero.length; i++) {
+          
+           for(int j=0; j < tablero.length; j++) {
+             if (tablero[i]==j){
+               System.out.print("☻ ");
+             }
+             else {
+               System.out.print ("☺ ");
+             }
+           }
+           System.out.println ("");
+     }
+    }
+    //funcion que dibuja una matriz con caras blancas(casillas vacias), y caras negras(con reinas)
     public static void main(String[] args) {
-        // TODO code application logic here
-        int semilla=111110;
-        int tpoblacion=5;
-        double pcruza=1.0;
-        double pmutaccion=1.0;
-        int iteraciones=10;
-        int ttablero=8;
+        // variables iniciales
+        int    semilla     ;           
+        int    tpoblacion  ;
+        double pcruza      ;
+        double pmutaccion  ;
+        int    iteraciones ;
+        int    ttablero    ;
         ArrayList<Integer> fitvalores = new ArrayList<>();
-      // Declaración el ArrayList de la poblacion
-        ArrayList<int []> poblacion = new ArrayList<>();//ponemos tableros a la poblacion 
-        for(int i=0;i<tpoblacion;i++){
-           int[] tablero= RandomizeArray(0, ttablero);
+        ArrayList<int []>  poblacion  = new ArrayList<>();//ponemos tableros a la poblacion  inicial
+        ArrayList<int []>  nuevagen  = new ArrayList<>();
+        Scanner sc=new Scanner(System.in);
+            System.out.println("Ingrese semilla: ");
+            semilla=sc.nextInt();
+            System.out.println("Ingrese numero de reinas ");
+            ttablero=(sc.nextInt()-1);
+            System.out.println("Ingrese tamaño de la poblacion: ");
+            tpoblacion=sc.nextInt();
+            System.out.println("Ingrese numero de generaciones: ");
+            iteraciones=sc.nextInt();
+            System.out.println("Ingrese probabilidad de cruza (con coma en un rango de 0,0 a 1,0): ");
+            pcruza=sc.nextDouble();
+            System.out.println("Ingrese probabilidad de mutacion (con coma en un rango de 0,0 a 1,0): ");
+            pmutaccion=sc.nextDouble();
+            System.out.println();
+            
+        //inicializamos la poblacion de tableros randomicamente 1ra generacion
+            for(int i=0;i<tpoblacion;i++){
+            int[] tablero= RandomizeArray(0, ttablero);
             poblacion.add(tablero);
             int fit=fittnes(tablero);
+            if (fit==0) {
+                  dibujo(tablero);
+                  break;
+            }
             fitvalores.add(fit);
-            System.out.println(Arrays.toString(tablero));
-            System.out.println( fit);
         }
-        //selecionamos dos padres por medio de la ruleta . solo obtenemos el iindice del tablero
-        int padre1=ruolette(fitvalores,poblacion, semilla);
-            System.out.println("padre 1 "+padre1);
-        int padre2=ruolette(fitvalores,poblacion, semilla);
         
-    
-          int semilla2=10;
-          while (padre2==padre1) {
+        
+        
+        for (int kk = 0; kk < tpoblacion/2; kk++) {
+        //selecionamos dos padres por medio de la ruleta . solo obtenemos el iindice del tablero
+        int padre1=ruolette(fitvalores,poblacion, semilla); 
+        int padre2=ruolette(fitvalores,poblacion, semilla);
+        // System.out.println("padre 1 "+padre1);
+        //volvemos a elegir el padre 2 si es igual al padre 1
+        int semilla2=10;
+        while (padre2==padre1) {
                padre2=ruolette(fitvalores,poblacion, semilla2);
                semilla2=semilla2*2;
-               //System.out.println(semilla2);
          //System.out.println("padre 2 "+ padre2);
-             }
+        }
        
-          System.out.println("padre 2 "+padre2);
-          double randomdeccc=randomadec(0,1,semilla);
-         // System.out.print("decimal ranmod "+randomdeccc);
-          boolean cruza;
-          cruza=probabilidadcruza(pcruza, randomdeccc);
-          //System.out.println("prob cruza "+cruza);
-          
-          //realiza la cruza¿
-          int mitad=randomanint(0,ttablero, semilla);
-          //recuperar padre y madre seleccionados
-          
-          System.out.println("mitad peruana "+mitad);
-          int [] padrek=obtenerpadre(poblacion, ttablero, padre1);
-          int [] padres=obtenerpadre(poblacion, ttablero, padre2);
-          int [] hijo1= new int[ttablero];;
-          int [] hijo2= new int[ttablero];;
-          
-          for (int i = 0; i <=ttablero; i++) {
-          //   hijo1[i]=0;
-        }
-          for (int i = 0; i <=ttablero; i++) {
-           // System.out.print( hijo1[i]);
-        }
-           System.out.println("--------------");
-         if(cruza==true){//realizamos la cruza
-           //  int []padre={2,2,2,2,2,2,2,2};
-            // int []madre={2,2,2,2,2,2,2,2};
-             // System.out.println("cruzamiento");
+        // System.out.println("padre 2 "+padre2);
+        //random decimal para ver si se cruzan los padres
+        double  randomdeccc=randomadec(0,1,semilla);
+        boolean cruza;
+        //obtenemos la probabilidad de cruza
+        cruza=probabilidadcruza(pcruza, randomdeccc);
+         //obtenemos la mitad para hacer a los hijos
+        int mitad=randomanint(0,ttablero, semilla);
+        //recuperar padre y madre seleccionados
+        int [] padrek=obtenerpadre(poblacion, ttablero, padre1);
+        int [] padres=obtenerpadre(poblacion, ttablero, padre2);
+        int [] hijo1= new int[ttablero];
+        int [] hijo2= new int[ttablero];
+        //realizamos la cruza
+        if(cruza==true){
               hijo1 =cruza( padrek, padres,  ttablero, mitad, 1);
               hijo2 =cruza( padrek, padres,  ttablero, mitad, 2);
-             
-         }
-          for (int i = 0; i <ttablero+1; i++) {
-            System.out.print( hijo1[i]);
         }
-          System.out.println(" ");
-           for (int i = 0; i <ttablero+1; i++) {
-            System.out.print( hijo2[i]);
-        }
-       System.out.println(" ");
-       
-       int[] hijoreparauno=reparacion2(hijo1,ttablero);
-       int[] hijoreparados=reparacion2(hijo2,ttablero);
-       int num1=randomanint(0, ttablero,  semilla);
-       int num2=randomanint(0, ttablero,  semilla);
-       int semilla3=10;
-          while (num1==num2) {
+        //  for (int i = 0; i <ttablero+1; i++) {
+           // System.out.print( hijo1[i]);
+      //  }
+        //  System.out.println(" ");
+        //   for (int i = 0; i <ttablero+1; i++) {
+         //   System.out.print( hijo2[i]);
+     //   }
+      // System.out.println(" ");
+        //reparamosa a los hijos si se repiten los numeros
+        int[] hijoreparauno=reparacion2(hijo1,ttablero);
+        int[] hijoreparados=reparacion2(hijo2,ttablero);
+        //seleccionamos numeros enteros para hacer la mutacion. si esta dentro de la probabilidad
+        int num1=randomanint(0, ttablero,  semilla);
+        int num2=randomanint(0, ttablero,  semilla);
+        //nos aseguramon que no salga el mismo numero
+        int semilla3=10;
+        while (num1==num2) {
                num2=randomanint(0, ttablero,  semilla3);
                semilla3=semilla3*2;
-               //System.out.println(semilla2);
-         //System.out.println("padre 2 "+ padre2);
-             }
-       System.out.println("numero 1 "+num1);
-       System.out.println("numero 2 "+num2);
-        double probmut=randomadec(0,1,semilla);
+        }
+        
+      // System.out.println("numero 1 "+num1);
+      // System.out.println("numero 2 "+num2);
+      //seleccionamos la probabilidad para la mutacion
+       double probmut=randomadec(0,1,semilla);
+       //hacemos la mutacion de los hijos
        int[] hijolistouno=mutacion(hijo1,ttablero,num1,num2,probmut,pmutaccion);
        int[] hijolistodos=mutacion(hijo2,ttablero,num1,num2,probmut,pmutaccion);
-   //se crea una nueva generacion
-   //  int tabo = randomabin(semilla);//tablero de 8
-   // double tabo2 = randomadec(0,10,semilla);
-        
-      // int[] tabo3= RandomizeArray(0, 10);
-      //  System.out.print(Arrays.toString(tabo3));
+       //agregamos lo hijos a la nueva generacion
+       //nuevagen.add(hijolistouno);
+      // nuevagen.add(hijolistodos);
+       poblacion.add(padre1, hijolistouno);
+       poblacion.add(padre2, hijolistodos);
+       int fit=fittnes(hijolistouno);
+            if (fit==0) {
+                  dibujo(hijolistouno);
+                  break;
+            }
+            fitvalores.add(padre1,fit);
+      //se crea una nueva generacion
+     }//fin llenado de nueva generacion
+   
     }
 
 }
